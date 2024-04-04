ff70df2beb7ed21e1947d1428f7cc6b9b2be2043
aryan <aryanhazarika04@gmail.com>
2024-04-04 21:55:53 +0530

    Deleted large img files from assets

--- src/Components/Sponsors/SponsorCards/SponsorCards.jsx
+++ src/Components/Sponsors/SponsorCards/SponsorCards.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './SponsorCardsStyles.css'
-import cards from './cards'
+import Cards from './Cards'
 
 function SponsorCards() {
   return (
     <div className='grid_container'>
-      {cards.map((item,i)=>(
+      {Cards.map((item,i)=>(
         <div className="card" key={item.id}>
           <img className='spons_img' src={item} alt="a sponsor" />
         </div>
