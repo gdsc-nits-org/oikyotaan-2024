eabe6f28898d988dfe98ca5a7a1dc83fb490b671
Dhiraj <rd6999787@gmail.com>
2024-04-05 23:20:24 +0530

    Team page complete for laptop view

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../Team/team.module.scss";
 import data from "../../../public/db/team.json";
 import { Card } from "../../Components/Team/Card";
 import Lottie from "lottie-react";
 import drumLeft from "../Team/drum right.json";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import  EffectCoverflow  from "swiper";
+import  Navigation  from "swiper";
+
 
 export const Team = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(true);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.header}>
@@ -29,11 +37,39 @@ export const Team = () => {
         {/* core */}
         <div className={styles.teamContainer}>
           <h3>CORE TEAM</h3>
-          <div className={styles.coreTeam}>
-            {data.core.members.map((member, index) => (
-              <Card member={member} key={index} />
-            ))}
-          </div>
+          {!isSmallScreen && (
+            <div className={styles.coreTeam}>
+              {data.core.members.map((member, index) => (
+                <Card member={member} key={index} />
+              ))}
+            </div>
+          )}
+
+          {/* swiper */}
+          {isSmallScreen && (
+            <Swiper
+              className={styles.coreTeam}
+              modules={ EffectCoverflow}
+              centeredSlides
+              slidesPerView={4}
+              spaceBetween={-20}
+              speed={500}
+              effect="coverflow"
+              rewind
+              coverflowEffect={{
+                rotate: 0,
+                stretch: 0,
+                depth: 200,
+                modifier: 2.5,
+              }}
+            >
+              {data.core.members.map((member, index) => (
+                <SwiperSlide key={index}>
+                  <Card member={member} />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          )}
         </div>
 
         {/* tech */}
