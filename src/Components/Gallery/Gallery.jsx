cc656bfd6274bc5ce780079fea64044f08999a0d
aryan <aryanhazarika04@gmail.com>
2024-04-07 01:37:45 +0530

    fixed minor errors

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,22 +1,13 @@
-import React, { useRef, useState, require } from 'react';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
-
 import './styles.css';
-
-// import required modules
 import { EffectCoverflow, Pagination, Autoplay } from 'swiper/core';
-
 let img1 = "https://res.cloudinary.com/dyn3q57b8/image/upload/f_auto,q_auto,fl_lossy/v1/Oikyotaan%20'24%20assets/Gallery/image1"
 let img2 = "https://res.cloudinary.com/dyn3q57b8/image/upload/f_auto,q_auto,fl_lossy/v1/Oikyotaan%20'24%20assets/Gallery/image2"
 let img3 = "https://res.cloudinary.com/dyn3q57b8/image/upload/f_auto,q_auto,fl_lossy/v1/Oikyotaan%20'24%20assets/Gallery/image3"
 import GalleryHeader from './GalleryHeader/GalleryHeader';
-
 
 
 
@@ -55,10 +46,6 @@ const Gallery = ()=> {
       <SwiperSlide><img src={img3} alt='Carousel photos'></img></SwiperSlide>
       <SwiperSlide><img src={img1} alt='Carousel photos'></img></SwiperSlide>
       <SwiperSlide><img src={img2} alt='Carousel photos'></img></SwiperSlide>
-{/* 
-      {GalleryImages.map((item)=>{
-        <SwiperSlide key={item.id}> <img src={item.imageAddress}></img> </SwiperSlide>
-      })} */}
 
       </Swiper>
 </div>
