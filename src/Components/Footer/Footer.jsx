3c984876485b64ed1c36e2ffecbf9241ba80dbf6
RISHI RAJ SAHA <132642857+rishinhk004@users.noreply.github.com>
2024-04-07 00:52:16 +0530

    About (#9)
    * fixes
    
    * link fix
    
    * nav-bar added and footer fixes
    
    * nav-bar added and footer fixes
    
    * nav-bar added and footer fixes
    
    * nav-bar added and footer fixes
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * comments removed
    
    * minor fixes and about sec add
    
    * brochure add
    
    * brochure add
    
    * brochure add
    
    * responsiveness fix
    
    * minor fixes
    
    * responsive fix and minor fixes
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -21,103 +21,113 @@ const Footer = () => {
       </div>
       <div className={styles.body}>
         <div className={styles.footer}>
-          <div className={styles.footerbody}>
-            <div className={styles.logo_box}>
-              <img
-                className={styles.logo}
-                alt="Oikyotaan Logo"
-                src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1712052815/Oikyotaan/ascrgnkddoyvanfs3ohr.svg"
-              />
-            </div>
-            <div className={styles.social}>
-              <div className={styles.contact}>
-                <div className={styles.contactinfo}>
-                  <img
-                    className={styles.logo2}
-                    src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1712052813/Oikyotaan/irum766hksgshwyvllax.svg"
-                    alt="location"
-                  />
-                  <p className={styles.contactdetails}>
-                    NIT Silchar, Assam, India 788010
-                  </p>
-                </div>
-                <div className={styles.contactinfo}>
-                  <img
-                    className={styles.logo2}
-                    src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1712052813/Oikyotaan/ae1w0uige95quqbamhhm.svg"
-                    alt="phone"
-                  />
-                  <div className={styles.contactdetails}>03842-228479</div>
-                </div>
+          <div className={styles.cont}>
+            <img
+              className={styles.imgtop}
+              src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1712057803/Oikyotaan/uhapwrvptmt7dsh2git1.webp"
+              alt="design"
+            />
+            <div className={styles.footerbody}>
+              <div className={styles.logo_box}>
+                <img
+                  className={styles.logo}
+                  alt="Oikyotaan Logo"
+                  src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1712419014/Oikyotaan/OIK_LOGO_COLOR_DROP_eavdpq.webp"
+                />
               </div>
-              <div className={styles.contact}>
-                <div className={styles.contactinfo}>
-                  <a href="https://www.facebook.com/nitsilcharoikyotaan" target="_blank">
+              <div className={styles.social}>
+                <div className={styles.contact}>
+                  <div className={styles.contactinfo}>
                     <img
                       className={styles.logo2}
-                      src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1712052813/Oikyotaan/zbrzxiuhtnz3g3szap26.svg"
-                      alt="facebook"
+                      src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1712052813/Oikyotaan/irum766hksgshwyvllax.svg"
+                      alt="location"
                     />
-                  </a>
-                  <div className={styles.contactdetails}>Facebook</div>
-                </div>
-                <div className={styles.contactinfo}>
-                  <a href="https://www.instagram.com/_oikyotaan_/?hl=en" target="_blank">
+                    <p className={styles.contactdetails}>
+                      NIT Silchar, Assam, India 788010
+                    </p>
+                  </div>
+                  <div className={styles.contactinfo}>
                     <img
                       className={styles.logo2}
-                      src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1712052813/Oikyotaan/juriiplrqf4bwf4jpsdn.svg"
-                      alt="instagram"
+                      src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1712052813/Oikyotaan/ae1w0uige95quqbamhhm.svg"
+                      alt="phone"
                     />
-                  </a>
-                  <div className={styles.contactdetails}>Instagram</div>
+                    <div className={styles.contactdetails}>03842-228479</div>
+                  </div>
                 </div>
-              </div>
-            </div>
-
-            <div className={styles.center}>
-              <div className={styles.gdsctext}>
-                DESIGNED AND DEVELOPED IN COLLABORATION WITH
-              </div>
-              <div className={styles.gdsctext_pos}>
-                <div className={styles.gdsclogo}>
-                  <img
-                    className={styles.animation}
-                    src="https://res.cloudinary.com/dagggqd6g/image/upload/f_auto,q_auto/hyg5xs9vkkpswxiiidp3"
-                    alt="gdsc_logo"
-                  />
-                </div>
-                <div className={styles.gdsc_box}>
-                  <div className={styles.gdsctext1}>GDSC NIT SILCHAR</div>
-                  <div className={styles.gdsclogoarow}>
+                <div className={styles.contact}>
+                  <div className={styles.contactinfo}>
                     <a
-                      href="https://gdscnits.in"
+                      href="https://www.facebook.com/nitsilcharoikyotaan"
                       target="_blank"
-                      rel="noopener noreferrer"
                     >
                       <img
-                        src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1712053442/Oikyotaan/uomxsgvmtfb7lttfszyz.svg"
-                        alt="arrow"
+                        className={styles.logo2}
+                        src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1712052813/Oikyotaan/zbrzxiuhtnz3g3szap26.svg"
+                        alt="facebook"
                       />
                     </a>
+                    <div className={styles.contactdetails}>Facebook</div>
+                  </div>
+                  <div className={styles.contactinfo}>
+                    <a
+                      href="https://www.instagram.com/_oikyotaan_/?hl=en"
+                      target="_blank"
+                    >
+                      <img
+                        className={styles.logo2}
+                        src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1712052813/Oikyotaan/juriiplrqf4bwf4jpsdn.svg"
+                        alt="instagram"
+                      />
+                    </a>
+                    <div className={styles.contactdetails}>Instagram</div>
                   </div>
                 </div>
               </div>
+
+              <div className={styles.center}>
+                <div className={styles.gdsctext}>
+                  DESIGNED AND DEVELOPED IN COLLABORATION WITH
+                </div>
+                <div className={styles.gdsctext_pos}>
+                  <div className={styles.gdsclogo}>
+                    <img
+                      className={styles.animation}
+                      src="https://res.cloudinary.com/dagggqd6g/image/upload/f_auto,q_auto/hyg5xs9vkkpswxiiidp3"
+                      alt="gdsc_logo"
+                    />
+                  </div>
+                  <div className={styles.gdsc_box}>
+                    <div className={styles.gdsctext1}>GDSC NIT SILCHAR</div>
+                    <div className={styles.gdsclogoarow}>
+                      <a
+                        href="https://gdscnits.in"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <img
+                          src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1712053442/Oikyotaan/uomxsgvmtfb7lttfszyz.svg"
+                          alt="arrow"
+                          className={styles.arrow}
+                        />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.png}>
+              <img
+                className={styles.imgbottom}
+                src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1712057803/Oikyotaan/uhapwrvptmt7dsh2git1.webp"
+                alt="design"
+              />
             </div>
           </div>
           <div className={styles.bottom}>
             <p className={styles.p}>All Rights Reserved ©Oikyotaan, NIT Silchar</p>
           </div>
-
-          <img
-            className={styles.imgtop}
-            src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1712057803/Oikyotaan/uhapwrvptmt7dsh2git1.svg"
-            alt="design"
-          />
-          <img
-            className={styles.imgbottom}
-            src="https://res.cloudinary.com/dgnlmdkyq/image/upload/v1712057870/Oikyotaan/uvhn2ccu3qwscip1zqme.svg"
-            alt="design"
-          />
         </div>
       </div>
     </>
