2216c549df0825ec116b7c60156fccfd0dcc4db2
aryan <aryanhazarika04@gmail.com>
2024-04-06 23:53:06 +0530

    ADD: Gallery Page

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,5 +1,6 @@
 import Home from "./Home/Home";
 import Error from "./Error/Error";
 import Artist from "./Artist/Artist";
+import Gallery from "./Gallery/Gallery"
 
-export { Home, Error, Artist };
+export { Home, Error, Artist, Gallery };
