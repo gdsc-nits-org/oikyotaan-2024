6eb4170f77ca3149648931399ce2e1d8629cb312
Dheeraj Das <rd6999787@gmail.com>
2024-04-06 22:59:51 +0530

    Team page (#10)
    * structure for team page ready
    
    * responsive team page need to be done
    
    * Team page complete for laptop view
    
    * minor changes needs to be done
    
    * minor changes need to be done
    
    * team page ready
    
    * minor changes on UI done
    
    * requested changes resolved
    
    * carousal UI issue resolved
    
    * carousal UI issue resolved

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,5 +1,6 @@
 import Home from "./Home/Home";
 import Error from "./Error/Error";
 import Artist from "./Artist/Artist";
+import Team from "./Team/Team.jsx";
 
-export { Home, Error, Artist };
+export { Home, Error, Artist, Team };
