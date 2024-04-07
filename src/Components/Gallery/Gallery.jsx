576292ff8b1b96b012493211294a5c4e6d20fa4a
aryan <91686553+aryanh0911@users.noreply.github.com>
2024-04-07 10:56:52 +0530

    ADD: Gallery Section (#8)
    * ADD: Sponsors section
    
    * Deleted large img files from assets
    
    * removed react imports from components
    
    * cleaned up SponsorsStyle.css
    
    * resolved merge conflict in home.jsx - 2
    
    * fixed an overflowing issue
    
    * fixed a linting error
    
    * added Snippet font
    
    * reduced card size
    
    * removed bg-image from the entire section
    
    * ADD: Gallery section
    
    * ADD: Gallery Section
    
    * Added lotties
    
    * fixed gradient transition of sponsor cards
    
    * added autoplay to swiper
    
    * ADD: Gallery Page
    
    * fixed swiper issues
    
    * deleted pnpm lock
    
    * deprecated swiper to ver. 9.1.1
    
    * fixed minor errors

---
+++ src/Components/Gallery/Gallery.jsx
@@ -0,0 +1,56 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import './styles.css';
+import { EffectCoverflow, Pagination, Autoplay } from 'swiper/core';
+let img1 = "https://res.cloudinary.com/dyn3q57b8/image/upload/f_auto,q_auto,fl_lossy/v1/Oikyotaan%20'24%20assets/Gallery/image1"
+let img2 = "https://res.cloudinary.com/dyn3q57b8/image/upload/f_auto,q_auto,fl_lossy/v1/Oikyotaan%20'24%20assets/Gallery/image2"
+let img3 = "https://res.cloudinary.com/dyn3q57b8/image/upload/f_auto,q_auto,fl_lossy/v1/Oikyotaan%20'24%20assets/Gallery/image3"
+import GalleryHeader from './GalleryHeader/GalleryHeader';
+
+
+
+const Gallery = ()=> {
+  return (
+    <div className='gallery-container'>  
+
+     <GalleryHeader />
+
+      <Swiper
+        effect={'coverflow'}
+        loop={true}
+        grabCursor={true}
+        spaceBetween={0}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: false,
+        }}
+
+        autoplay={{
+          delay: 1500,
+          disableOnInteraction: false,
+        }}
+        
+        pagination={true}
+        modules={[EffectCoverflow, Pagination, Autoplay]}
+        className="mySwiper"
+      >
+      <SwiperSlide><img src={img1} alt='Carousel photos'></img></SwiperSlide>
+      <SwiperSlide><img src={img2} alt='Carousel photos'></img></SwiperSlide>
+      <SwiperSlide><img src={img3} alt='Carousel photos'></img></SwiperSlide>
+      <SwiperSlide><img src={img1} alt='Carousel photos'></img></SwiperSlide>
+      <SwiperSlide><img src={img2} alt='Carousel photos'></img></SwiperSlide>
+
+      </Swiper>
+</div>
+  );
+}
+
+
+export default Gallery;
