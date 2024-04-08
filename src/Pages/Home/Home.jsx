a9ffe978401c09779634567b0d7af7af3d51bc90
RISHI RAJ SAHA <132642857+rishinhk004@users.noreply.github.com>
2024-04-08 11:39:48 +0530

    Some Changes (#13)
    * fixes
    
    * link fix
    
    * nav-bar added and footer fixes
    
    * nav-bar added and footer fixes
    
    * nav-bar added and footer fixes
    
    * nav-bar added and footer fixes
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * comments removed
    
    * minor fixes and about sec add
    
    * brochure add
    
    * brochure add
    
    * brochure add
    
    * responsiveness fix
    
    * minor fixes
    
    * responsive fix and minor fixes
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * scroll fix
    
    * scroll fix
    
    * minor fixes and Event page fix
    
    * minor fixes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,6 @@ import {Gallery, Sponsors, AboutUs, AboutNits} from '../../Components'
 const Home = () => {
   return (
     <main className={styles.home}>
-      <h1>Oikyotaan 2024</h1>
       <AboutUs />
       <AboutNits />
       <Sponsors />
