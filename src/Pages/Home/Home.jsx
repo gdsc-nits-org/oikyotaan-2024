ab0c122d2898ae2a5ce9914681b04782928e0474
RISHI RAJ SAHA <132642857+rishinhk004@users.noreply.github.com>
2024-04-08 23:07:03 +0530

    Changes (#15)
    * fixes
    
    * link fix
    
    * nav-bar added and footer fixes
    
    * nav-bar added and footer fixes
    
    * nav-bar added and footer fixes
    
    * nav-bar added and footer fixes
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * comments removed
    
    * minor fixes and about sec add
    
    * brochure add
    
    * brochure add
    
    * brochure add
    
    * responsiveness fix
    
    * minor fixes
    
    * responsive fix and minor fixes
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * NewEvents commit
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * scroll fix
    
    * scroll fix
    
    * Events Page Added
    
    * minor fixes and Event page fix
    
    * minor fixes
    
    * Events Fixed
    
    * Loader task
    
    * Loader added
    
    * Loader added
    
    * minor fixes
    
    * minor fixes and favicon added
    
    * minor fixes and favicon added
    
    * logo changed
    
    * logo changed
    
    ---------
    
    Co-authored-by: Arindom <arindom956@gmail.com>

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,14 @@
 import styles from "./Home.module.scss";
 import {Gallery, Sponsors, AboutUs, AboutNits} from '../../Components'
+import { useEffect } from "react";
 import Hero from "../../Components/Hero/Hero";
+
 const Home = () => {
+
+  useEffect(() => {
+    window.scrollTo(0, 0); 
+  }, []);
+
   return (
     <main className={styles.home}>
       <Hero/>
--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -69,6 +69,9 @@ const Team = () => {
       </Swiper>
     );
   };
+  useEffect(() => {
+    window.scrollTo(0, 0); 
+  }, []);
 
   return (
     <div className={styles.wrapper}>
